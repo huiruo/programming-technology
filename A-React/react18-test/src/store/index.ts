0958839142c6380eb7991f855e12ae5bd3534344
Ruome <2196411859@qq.com>
2023-01-04 00:26:50 +0800

    feat: redux test project

---
+++ A-React/react18-test/src/store/index.ts
@@ -0,0 +1,19 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+// import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+import userStore from './user.store';
+// import { composeWithDevTools } from 'redux-devtools-extension';
+
+const reducer = combineReducers({
+  userStore,
+});
+
+// const enhancers = process.env.APP_ENV === 'dev' ? composeWithDevTools(
+//   applyMiddleware(thunk, logger)
+// ) : applyMiddleware(thunk);
+
+const enhancers = applyMiddleware(thunk)
+
+const store: any = createStore(reducer, enhancers);
+
+export default store;
